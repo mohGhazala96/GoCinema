7a9749eac21448eeccfc6e827a29a7d04be1526d
Farid <farid.abdelhalim@gmail.com>
2018-12-02 22:51:36 +0200

    Functionality of Home Page is working

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReserveComponent } from './reserve/reserve.component';
+import { MoviesViewComponent } from './movies-view/movies-view.component' 
 
 const routes: Routes = [
   {
     path: 'reserve',
     component: ReserveComponent
+  }, 
+
+  {
+    path: 'moviesview' ,
+    component: MoviesViewComponent
   }
 ];
 
--- frontend/src/app/movies-view/movies-view.component.ts
+++ frontend/src/app/movies-view/movies-view.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+export class Movie {
+  Id : Number ;
+  Vote_average : Number;
+  Title : string;
+  Poster_path : string;
+  Overview : string;
+  Release_date : string
+};
 
 @Component({
   selector: 'app-movies-view',
@@ -7,9 +18,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesViewComponent implements OnInit {
 
-  constructor() { }
+  movies : Movie[];
+
+  constructor(private router : Router, private activatedRoute: ActivatedRoute, private httpClient: HttpClient) { 
+
+
+  }
+
+  getMovies() {
+
+    var config = {
+      headers:
+          {
+              'Content-Type': 'application/json'
+          }
+    }  
+
+    this.httpClient.get('http://localhost:3000/api/getMovies').subscribe(
+
+      res => {
+        
+
+        for( var i = 0; i < 20; i++) {
+
+          this.movies[i] = new Movie() 
+          this.movies[i].Id = res['Movies'][i].Id
+          this.movies[i].Vote_average = res['Movies'][i].Vote_average
+          this.movies[i].Title = res['Movies'][i].Title
+          this.movies[i].Poster_path = res['Movies'][i].Poster_path
+          this.movies[i].Overview = res['Movies'][i].Overview
+          this.movies[i].Release_date = res['Movies'][i].Release_date
+
+        }
+      }
+    );
+
+  }
+
+  reserveSeat( movieId: Number){
+
+   window.location.href = "/reserve?movie_id=" + movieId
+  }
 
   ngOnInit() {
+    this.movies = new Array(20)
+    this.getMovies()
+    console.log(this.movies)
   }
 
 }
